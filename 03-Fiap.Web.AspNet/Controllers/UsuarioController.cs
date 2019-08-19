3b12c08309345abc8b1e79d8d54321dee478f7bf
Logon Aluno <logonaluno@local.com>
2019-08-19 11:36:58 -0300

    Aplicacao ASP

---
+++ 03-Fiap.Web.AspNet/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _03_Fiap.Web.AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _03_Fiap.Web.AspNet.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult UsuarioCadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UsuarioCadastrar(Usuario usuario)
+        {
+            ViewBag.usuario = usuario.Name;
+            ViewData["data"] = usuario.DataNascimento;
+            TempData["msg"] = "sucesso!";
+            return View(usuario);
+        }
+    }
+}
